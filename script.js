8cad5cf3bdbc174ca63a51f2411725a63c0180b8
Armaan Moosani <armaan.moosani9035@.com>
2025-10-08 20:14:15 -0500

    added color

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const button= document.getElementById("showPic");
+const form = document.querySelector("form");
 button.addEventListener('click',()=>{
     document.getElementById("dak").style.display = "block";
 })
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+  document.getElementById("form").style.backgroundColor = "rgba(142, 190, 171, 1)";
+});
